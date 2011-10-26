333e85c4c729c4c30ee561bc782f21f1dc170199
shuohuan chang <jsc9530@gmail.com>
2011-10-26 01:12:18 -0400

    my final

---
+++ Project-2_Code.pro
@@ -0,0 +1,47 @@
+#-------------------------------------------------
+#
+# Project created by QtCreator 2011-10-15T12:09:18
+#
+#-------------------------------------------------
+
+
+TARGET = runme.out
+CONFIG   += console
+CONFIG   -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
+
+
+documentation.path = ~/Documents
+documentation.files = docs/*
+
+INSTALLS +=documentation
+#QMAKE_CXX = g++-4.6
+QMAKE_CXX = g++
+# QMAKE_CXXFLAGS = --std=c++0x
+
+HEADERS += \
+    vspcap.h \
+    inputfiles.h \
+    vsgeneralexception.h \
+    callbackfunc.h \
+    project2report.h \
+    p538project2.h
+
+LIBS -= -lQtGui -lQtCore -lpthread
+LIBS += -lpcap
+INCLUDEPATH += /myfiles/LinuxProject/verbalsaint/include
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ main.cpp
@@ -0,0 +1,54 @@
+/*
+  P538 Project 2
+  2011 Oct 17
+  Team member : ShuoHuan Chang , Sachin Joshi
+
+  FrameWork : ShuoHuan Chang
+  Please refer to README to see the task dispatch for each team member.
+*/
+
+#include "verbalsaint.h"
+#include "vsgeneralexception.h"
+#include "p538project2.h"
+#include "inputfiles.h"
+#include "vspcap.h"
+
+UV(VSPCAP)
+UV(VSEXCEPTION)
+using namespace PROJECT2;
+
+int main()
+{
+    using namespace std;
+    P2Files Test;
+    cout << Test.cFile_Dhcp << endl;
+    cout << Test.cFile_Tracerout << endl;
+    cout << Test.cFile_Wget << endl;
+    //------------
+    VSPcap PcapWget(P2Files().cFile_Wget);
+    cout << "Check with pcap_datalink for file " << P2Files().cFile_Wget << " : " << PcapWget.checkPcap_datalink() << endl;
+
+    // and \\tcp or \\udp
+    PcapWget.compile("arp or ip or tcp or udp");
+    PcapWget.Start();
+
+    //------------
+    VSPcap PcapTracerout(P2Files().cFile_Tracerout);
+    cout << "Check with pcap_datalink for file " << P2Files().cFile_Tracerout << " : " << PcapTracerout.checkPcap_datalink() << endl;
+
+    // and \\tcp or \\udp
+    PcapTracerout.compile("arp or ip or tcp or udp");
+    PcapTracerout.Start();
+
+    //------------
+    VSPcap PcapDhcp(P2Files().File_Dhcp);
+    cout << "Check with pcap_datalink for file " << P2Files().File_Dhcp << " : " << PcapDhcp.checkPcap_datalink() << endl;
+
+
+    // and \\tcp or \\udp
+    PcapDhcp.compile("arp or ip or tcp or udp");
+    PcapDhcp.Start();
+    //------------
+    return 0;
+}
+
